1ca509a2d113b00f3334d1053e2cdaa401c9680c
Michele Beckmann <m.beckmann@live.de>
2022-11-30 23:32:33 +0100

    Add project files.

---
+++ AdventOfCode.fs.Tests/2021/Day1Tests.fs
@@ -0,0 +1,17 @@
+﻿module Day1Tests
+
+open System
+open Xunit
+open AdventOfCode.fs.Year2021
+
+[<Fact>]
+let ``Part 1`` () =
+    
+    let report =  [ 199; 200; 208; 210; 200; 207; 240; 269; 260; 263 ]
+    Assert.Equal(7, Day1.SolvePart1 report)
+
+[<Fact>]
+let ``Part 2`` () =
+    
+    let report =  [ 199; 200; 208; 210; 200; 207; 240; 269; 260; 263 ]
+    Assert.Equal(5, Day1.SolvePart2 report)
---
+++ AdventOfCode.fs/2021/Day1.fs
@@ -0,0 +1,30 @@
+﻿namespace AdventOfCode.fs.Year2021
+
+module Day1 =
+    open System.Collections.Generic
+    open System.Linq 
+
+    let SolvePart1 report =
+        
+        let mutable increases = 0
+        let mutable pre = 0
+
+        for value in report do
+            if value > pre && pre > 0 
+            then increases <- increases+1
+            pre <- value
+        
+        increases
+
+
+    let SolvePart2 (report: list<int>) =
+        
+        let sums = new List<int>()
+
+        report
+        |> Seq.iteri (fun index current ->
+            let sum = current + report.Skip(index+1).FirstOrDefault() + report.Skip(index+2).FirstOrDefault()
+            sums.Add(sum)
+        )
+
+        SolvePart1(sums)
