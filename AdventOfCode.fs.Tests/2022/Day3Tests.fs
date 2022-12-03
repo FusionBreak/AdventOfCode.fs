6eecade679202efcaa6a3dd4b6ceba053756c182
Michele Beckmann <m.beckmann@live.de>
2022-12-03 20:49:02 +0100

    solve day 3 part 1 & 2 2022

---
+++ AdventOfCode.fs.Tests/2022/Day3Tests.fs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.fs.Tests.Year2022
+
+open Xunit
+open AdventOfCode.fs.Year2022
+open System.IO
+
+module Day3Tests =
+
+
+    [<Fact>]
+    let ``Part 1`` () =
+
+        let report = File.ReadLines("Input/2022/Day3.txt") |> Seq.toList
+        Assert.Equal(8202, Day3.SolvePart1 report)
+
+    [<Fact>]
+    let ``Part 2`` () =
+
+        let report = File.ReadLines("Input/2022/Day3.txt") |> Seq.toList
+        Assert.Equal(2864, Day3.SolvePart2 report)
---
+++ AdventOfCode.fs/2022/Day3.fs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode.fs.Year2022
+
+open System
+
+module Day3 =
+
+    let toPoints (letter:char) =
+            if Char.IsLower letter then
+                int letter - 96
+            else
+                int letter - 38
+
+    let SolvePart1 input =
+        let halveRow (input:string) =
+            let mid = input.Length / 2
+            (input[..mid-1], input[mid..])
+
+        let findDuplicate ((head:string), (tail:string)) : char =
+            head
+            |> Seq.where (fun letter -> tail.Contains(letter))
+            |> Seq.head
+
+        input
+        |> Seq.map (fun row -> halveRow row)
+        |> Seq.map (fun rowHalves -> findDuplicate rowHalves)
+        |> Seq.map (fun duplicate -> toPoints duplicate)
+        |> Seq.sum
+
+
+    let SolvePart2 input =
+        let chunkRows (rows:list<string>) =
+            seq {
+                let mutable index = 0
+
+                while index < rows.Length do
+                    yield rows |> Seq.skip index |> Seq.take 3
+                    index <- index + 3
+            }
+
+        let findDuplicate (rows:seq<string>) =
+            let first = rows |> Seq.head
+            let second = rows |> Seq.skip 1 |> Seq.head
+            let third = rows |> Seq.skip 2 |> Seq.head
+            
+            first
+            |> Seq.where (fun letter -> second.Contains(letter) && third.Contains(letter))
+            |> Seq.head
+
+        input
+        |> chunkRows
+        |> Seq.map (fun chunk -> findDuplicate chunk)
+        |> Seq.map (fun duplicate -> toPoints duplicate)
+        |> Seq.sum
+            
