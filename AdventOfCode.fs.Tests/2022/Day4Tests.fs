23d64001a22a22cee0297e99c163aa7db7c2a1ed
Michele Beckmann <m.beckmann@live.de>
2022-12-04 21:24:15 +0100

    solve day 4 part 1 & 2 2022

---
+++ AdventOfCode.fs.Tests/2022/Day4Tests.fs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.fs.Tests.Year2022
+
+open Xunit
+open AdventOfCode.fs.Year2022
+open System.IO
+
+module Day4Tests =
+
+
+    [<Fact>]
+    let ``Part 1`` () =
+
+        let report = File.ReadLines("Input/2022/Day4.txt") |> Seq.toList
+        Assert.Equal(571, Day4.SolvePart1 report)
+
+    [<Fact>]
+    let ``Part 2`` () =
+
+        let report = File.ReadLines("Input/2022/Day4.txt") |> Seq.toList
+        Assert.Equal(917, Day4.SolvePart2 report)
---
+++ AdventOfCode.fs/2022/Day4.fs
@@ -0,0 +1,41 @@
+﻿namespace AdventOfCode.fs.Year2022
+
+open System
+
+module Day4 =
+
+    let parse (row:string) =
+        row.Split ','
+        |> Seq.map (fun part ->
+                        let numbers =part.Split '-'
+                        (int numbers[0], int numbers[1])
+                    )
+    
+    let isInRange slice number =
+            slice |> Seq.contains number
+
+    let containsCompletely a b =
+        let (aFirst, aLast) = a
+        let (bFirst, bLast) = b
+        aFirst >= bFirst && aLast <= bLast || bFirst >= aFirst && bLast <= aLast
+
+    let overlaps a b =
+        let (aFirst, aLast) = a
+        let (bFirst, bLast) = b
+        aFirst |> isInRange [bFirst..bLast] || aLast |> isInRange [bFirst..bLast] 
+        ||
+        bFirst |> isInRange [aFirst..aLast] || bLast |> isInRange [aFirst..aLast]
+        
+    let SolvePart1 input =
+        input
+        |> Seq.map (fun row -> parse row)
+        |> Seq.map (fun parts -> containsCompletely (parts |> Seq.head) (parts |> Seq.last))
+        |> Seq.where (fun result -> result)
+        |> Seq.length
+
+    let SolvePart2 input =
+        input
+        |> Seq.map (fun row -> parse row)
+        |> Seq.map (fun parts -> overlaps (parts |> Seq.head) (parts |> Seq.last))
+        |> Seq.where (fun result -> result)
+        |> Seq.length
