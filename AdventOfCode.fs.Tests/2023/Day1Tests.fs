81af5a33830d523e253469517876c0d16e0c7de9
Michele Beckmann <m.beckmann@live.de>
2023-12-01 21:15:23 +0100

    solve day 1 part 1 and part 2 is WIP 2023

---
+++ AdventOfCode.fs.Tests/2023/Day1Tests.fs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode.fs.Tests.Year2023
+
+open Xunit
+open AdventOfCode.fs.Year2023
+open System.IO
+
+module Day1Tests =
+
+
+    [<Fact>]
+    let ``Part 1`` () =
+        let report = File.ReadLines("Input/2023/Day1.txt") |> Seq.toList
+        Assert.Equal(54304, Day1.solvePart1 report)
+
+    [<Fact>]
+    let ``Part 2`` () =
+        let report = File.ReadLines("Input/2023/Day1.txt") |> Seq.toList
+        Assert.Equal(53892, Day1.solvePart2 report)
---
+++ AdventOfCode.fs/2023/Day1.fs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode.fs.Year2023
+
+open System
+
+module Day1 =
+    type namedNumber = {
+        Name: string
+        Number: int
+    }
+    
+    let allNamedNumbers = 
+        [|
+            { Name = "zero"; Number = 0 }
+            { Name = "one"; Number = 1 }
+            { Name = "two"; Number = 2 }
+            { Name = "three"; Number = 3 }
+            { Name = "four"; Number = 4 }
+            { Name = "five"; Number = 5 }
+            { Name = "six"; Number = 6 }
+            { Name = "seven"; Number = 7 }
+            { Name = "eight"; Number = 8 }
+            { Name = "nine"; Number = 9 }
+        |]
+
+    let allallNamedNumbersReversed = 
+        allNamedNumbers
+        |> Array.rev
+    
+    let replaceAllNamesToNumbers (input: string) =
+        let mutable result = input
+        for namedNumber in allallNamedNumbersReversed do
+            result <- result.Replace(namedNumber.Name, namedNumber.Number.ToString())
+        result
+    
+    let firstAndLast seq = 
+        Seq.head seq, Seq.last seq
+
+    let combineFirstAndLastDigit (first, last) = 
+        let first = int (first - '0')
+        let last = int (last - '0')
+        first * 10 + last
+
+    let getCoordinate (input: string) =
+        input
+        |> Seq.where Char.IsDigit
+        |> firstAndLast
+        |> combineFirstAndLastDigit
+
+    let solvePart1 input =
+        input
+        |> Seq.map getCoordinate
+        |> Seq.sum
+
+    let solvePart2 input =
+        input
+        |> Seq.map replaceAllNamesToNumbers
+        |> Seq.map getCoordinate
+        |> Seq.toList
+        |> Seq.sum
+        
+        
+
